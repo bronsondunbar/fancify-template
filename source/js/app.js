4c633ad41604a78453a9ca72a26ee75f53a7e80a
Bronson Dunbar <bronson@bronsondunbar.com>
2018-04-24 19:41:56 +0200

    Setup commit

---
+++ source/js/app.js
@@ -0,0 +1,9 @@
+
+// @codekit-prepend 'lib/jquery.1.12.4.js'
+// @codekit-prepend 'lib/jquery.mobile.1.4.5.js'
+// @codekit-prepend 'lib/bootstrap.bundle.js'
+// @codekit-prepend 'lib/jquery.color.js'
+// @codekit-prepend 'lib/jquery.waypoints.js'
+// @codekit-prepend 'lib/velocity.js'
+// @codekit-prepend 'lib/velocity.ui.js'
+
